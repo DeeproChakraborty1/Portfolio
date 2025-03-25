c6f422b85028e95c3394914c5ad73f7918a9ff8c
Deepro Chakraborty <deepro_19@rediffmail.com>
2025-03-26 01:05:56 +0530

    done

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+function downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'assets/CV.pdf';
+    link.download = 'CV.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }  
